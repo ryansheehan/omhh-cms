5221dae3ce9636828834eb43de9de4e81700c305
Ryan Sheehan <rsheehan@gmail.com>
2021-05-23 17:53:21 -0500

    food creation api complete

--- api/food/controllers/food.js
+++ api/food/controllers/food.js
@@ -1,116 +1,49 @@
 'use strict';
 
-const sqlite3 = require('sqlite3').verbose();
-const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+const { sanitizeEntity } = require('strapi-utils');
+
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-function LoadDatabaseAsync(path) {
-  return new Promise((resolve, reject) => {
-    const db = new sqlite3.Database(
-      `${path}`,
-      sqlite3.OPEN_READONLY,
-      err => err ? reject(err) : resolve(db)
-    );
-  });
-}
-
-function baseQuery(fn, strings, tokens) {
-  const out = [strings[0]];
-  for(let i = 1; i < strings.length; i++) {
-    out.push(tokens[i-1], strings[i]);
-  }
-  const query = out.join('');
-  return new Promise((resolve, reject) => {
-    fn(query, (err, rows) => err ? reject(err) : resolve(rows));
-  });
-}
-
 module.exports = {
+  async findOne(ctx) {
+    const food = await strapi.query('food').findOne({'fdc_id': ctx.params.fdc_id});
+    if (food) {
+      return sanitizeEntity(food, { model: strapi.models.food });
+    } else {
+      ctx.status = 404;
+      ctx.body = 'not found';
+    }
+  },
+
+  async update(ctx) {
+    const { fdc_id } = ctx.params;
+
+    entity = await strapi.services.food.update({ fdc_id }, ctx.request.body);
+
+    return sanitizeEntity(entity, { model: strapi.models.food });
+  },
+
+  async delete(ctx) {
+    const { fdc_id } = ctx.params;
+    const entity = await strapi.query('restaurant').delete({fdc_id})
+    return sanitizeEntity(entity, { model: strapi.models.food });
+  },
+
   async create(ctx) {
     const foods = ctx.request.body;
     try {
-
+      const results = await strapi.services.food.upsertMany(foods);
+      results.created = results.created.map(f => sanitizeEntity(f, {model: strapi.models.food}));
+      results.updated = results.updated.map(f => sanitizeEntity(f, {model: strapi.models.food}));
+      return results;
     } catch(err) {
       strapi.log.error(err);
       ctx.status = 500;
       ctx.body = 'unable to process foods';
     }
-  }
-
-  // async create(ctx) {
-  //   if (ctx.is('multipart')) {
-  //     const {data, files} = parseMultipartData(ctx);
-
-  //     let db = undefined;
-  //     try {
-  //       db = await LoadDatabaseAsync(files.db.path);
-  //     } catch (err) {
-  //       return ctx.badRequest('Unable to open database');
-  //     }
-
-  //     function all(strings, ...tokens) {
-  //       return baseQuery((q, cb) => db.all(q, cb), strings, tokens);
-  //     }
-
-  //     function get(strings, ...tokens) {
-  //       return baseQuery((q, cb) => db.get(q, cb), strings, tokens);
-  //     }
-
-  //     try {
-  //       // delete old food data
-  //       await Promise.all([
-  //         strapi.services.food.deleteAllButCustom(),
-  //         strapi.services.nutrient.deleteAll(),
-  //       ]);
-
-  //       // populate nutrient info
-  //       const nutrientTable = await all`SELECT name, unit_name, id AS nutrient_id FROM nutrient`;
-  //       const nutrientResults = await strapi.query('nutrient').createMany(nutrientTable);
-
-  //       // create nutrient lookup table
-  //       const nutrientLookUp = nutrientResults.reduce((dict, n) => {
-  //         const {nutrient_id, id} = n;
-  //         dict[nutrient_id] = id;
-  //         return dict;
-  //       }, {});
-
-  //       const foodIds = await all`SELECT fdc_id FROM food`;
-  //       const batchSize = 100;
-  //       for(let i = 0; i < foodIds.length; i += batchSize) {
-  //         const foods = [];
-  //         for(let j = 0; j < 100 && i + j < foodIds.length; j++) {
-  //           const {fdc_id} = foodIds[i + j];
-  //           const foodPromise = get`SELECT fdc_id, description, data_type AS source FROM food WHERE fdc_id = ${fdc_id}`;
-  //           const nutrientsPromise = all`
-  //             SELECT fn.amount, fn.nutrient_id
-  //             FROM food_nutrient fn
-  //             WHERE fn.fdc_id = ${fdc_id}`;
-  //           const [food, nutrients] = await Promise.all([foodPromise, nutrientsPromise]);
-  //           const strapiFood = {
-  //             ...food,
-  //             nutrients: nutrients.map(({amount, nutrient_id}) => ({amount, nutrient: nutrientLookUp[nutrient_id]}))
-  //           };
-  //           foods.push(strapiFood);
-  //         }
-  //         const results = await strapi.query('food').createMany(foods);
-  //         let junk = 0;
-  //         junk += 1;
-  //       }
-  //     } catch (err) {
-  //       return ctx.badRequest({
-  //         error: 'Invalid data.',
-  //         details: err,
-  //       });
-  //     }
-
-  //   } else {
-  //     return ctx.badRequest({
-  //       error: 'Invalid data.',
-  //     });
-  //   }
-  // }
+  },
 };
--- api/food/services/food.js
+++ api/food/services/food.js
@@ -25,8 +25,90 @@ module.exports = {
     }
   },
 
-  async upsertFoods(foods) {
-    const fdc_ids = foods.map(f => f.fdc_id);
-    // const current = await strapi.query('food').
+  async update(params, data) {
+    const existingEntry = await strapi.query('food').findOne(params);
+
+    const validData = await strapi.entityValidator.validateEntityUpdate(
+      strapi.models.food,
+      data,
+    );
+
+    const entry = await strapi.query('food').update(params, validData);
+
+    return entry;
+  },
+
+  async upsertMany(foods) {
+    const errors = [];
+
+    const validatedPromises = await Promise.allSettled(
+      foods.map(food => strapi.entityValidator.validateEntityUpdate(strapi.models.food, food))
+    );
+
+    errors.concat(
+      validatedPromises
+        .filter(({status}) => status === 'rejected')
+        .map(({reason}) => reason)
+    );
+
+    const newFoods = validatedPromises
+      .filter(({status}) => status === 'fulfilled')
+      .map(({value}) => value)
+      .reduce((col, food) => {
+        col[food.fdc_id] = food;
+        return col;
+      }, {});
+
+    const fdc_ids = Object.keys(newFoods);
+
+    // find existing foods
+    const foodsToBeUpdatedQuery = await strapi.query('food')
+      .model.query(qb => {
+        qb.where('fdc_id', 'in', fdc_ids)
+      })
+      .fetchAll();
+
+    const foodsToBeUpdated = foodsToBeUpdatedQuery.toJSON();
+
+    const existingFoods = foodsToBeUpdated.reduce((col, food) => {
+      col[food.fdc_id] = food;
+      return col;
+    }, {});
+
+    // find foods that do not already exist
+    const foodsToAdd = [];
+    fdc_ids.forEach(fid => {
+      if (! (fid in existingFoods)) {
+        foodsToAdd.push(newFoods[fid]);
+      }
+    });
+
+    // update existing foods
+    const updatedPromises = await Promise.allSettled(foodsToBeUpdated.map(
+      ({id, fdc_id}) => strapi.query('food').update({id}, newFoods[fdc_id])
+    ));
+
+    // collect errors
+    errors.concat(
+      updatedPromises
+        .filter(({status}) => status === 'rejected')
+        .map(({reason}) => reason)
+    );
+
+    // get finalized results
+    const foodsUpdated = updatedPromises
+      .filter(({status}) => status === 'fulfilled')
+      .map(({value}) => value);
+
+
+    // add new foods
+    const addedFoods = await strapi.query('food').createMany(foodsToAdd);
+
+    // return results
+    return {
+      created: addedFoods,
+      updated: foodsUpdated,
+      errors,
+    };
   }
 };
